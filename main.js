92b3b3856b29f32f0fb933510f2b7c8172841b18
Simone Guarneri <simone.guarneri.s@gmail.com>
2022-04-01 18:52:38 +0200

    Finished all Fetching

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
 import './style.css'
+
+async function fetchDataFromAPIEndpoint(){
+
+    const cards = await fetch("/api/fetchNotion")
+        .then((response) => response.json()
+        .then((data) => data.results)
+    );
+
+    document.querySelector('.card-container').innerHTML = cards.map((card) => `
+        <article class="card">
+            <img
+                src="${card.properties.Image.files[0].external.url}"
+                alt="${card.properties.Name.title[0].plain_text}"
+                class="card-img"
+            />
+            <h2 class="card-heading">${card.properties.Name.title[0].plain_text}</h2>
+
+            <div class="card-content">
+                <p>
+                   ${card.properties.Content.rich_text[0].plain_text} 
+                </p>
+            </div>
+
+            <a href="${card.properties.Link.url}" class="card-btn">${card.properties.Btn_Text.rich_text[0].plain_text}</a>
+        </article>
+    `).join('');
+
+}
+
+fetchDataFromAPIEndpoint();
